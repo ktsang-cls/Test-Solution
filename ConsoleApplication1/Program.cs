9b66df24e71a452e0dad6400390a2dc2b6328ff8
Keith.Tsang@theco-operativelegal.co.uk <AngelSquare$66>
2015-04-21 13:52:35 +0100

    Test

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            // Test
             Console.WriteLine("Version: " + Environment.Version.ToString());
             Console.ReadKey();
         }
